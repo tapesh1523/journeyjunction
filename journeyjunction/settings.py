8bf54814db218d2409feda932dc93ee6ca5deef2
Tapesh Sharma <64701641+tapesh1523@users.noreply.github.com>
2024-06-15 09:59:48 +0530

    configure static files handling for deployment

--- journeyjunction/settings.py
+++ journeyjunction/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "journeyjunction.urls"
@@ -128,3 +129,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
